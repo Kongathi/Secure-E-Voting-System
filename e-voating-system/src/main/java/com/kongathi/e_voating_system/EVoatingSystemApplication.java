935707f709b450e219c78ebfa910b5e4954fd9b9
Rohith Kongathi <kongathirohith@gmail.com>
2025-06-11 18:11:15 +0530

    Initial Spring Boot Project Setup

---
+++ e-voating-system/src/main/java/com/kongathi/e_voating_system/EVoatingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kongathi.e_voating_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVoatingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVoatingSystemApplication.class, args);
+	}
+
+}
---
+++ e-voating-system/src/test/java/com/kongathi/e_voating_system/EVoatingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kongathi.e_voating_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVoatingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
